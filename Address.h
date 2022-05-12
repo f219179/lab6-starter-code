be2da9efd5374c5ebc97ff850d1f652c1459fe14
Muhammad Azeem <90174299+mazeem-fast@users.noreply.github.com>
2022-05-12 14:31:34 +0500

    Update Address.h

--- Address.h
+++ Address.h
@@ -12,7 +12,7 @@ private:
 public:
 	bool equals(const Address& address);
 	void print_address();
-	Address* copy_address();
+	Address copy_address();
 
 	Address(std::string house, std::string street, std::string city, std::string country);
 	Address() {}
