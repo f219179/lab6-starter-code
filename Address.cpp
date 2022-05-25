8bb9691a31a9243f3509c971834766d98ed96df3
f219179 <101401415+f219179@users.noreply.github.com>
2022-05-26 01:39:57 +0500

    Update Address.cpp

--- Address.cpp
+++ Address.cpp
@@ -1,19 +1,49 @@
 #include "Address.h"
-
-
 /*
 *	Implement a constructor that initializes all the address parts
 *
 */
-
-
+Address::Address(std::string house, std::string street, std::string city, std::string country) {
+	this->house = house;
+	this->street = street;
+	this->city = city;
+	this->country = country;
+}
 /*
 *	Implement getter/setters for all the member variables of Address. Also declare them in the Address.h header file
 *
 */
-
-
-
+void Address::set_house(std::string house)
+{
+	this->house = house;
+}
+void Address::set_street(std::string street)
+{
+	this->street = street;
+}
+void Address::set_city(std::string city)
+{
+	this->city = city;
+}
+void Address::set_country(std::string country) {
+	this->country = country;
+}
+std::string Address::get_house()
+{
+	return house;
+}
+std::string Address::get_street()
+{
+	return street;
+}
+std::string  Address::get_city()
+{
+	return city;
+}
+std::string Address::get_country()
+{
+	return country;
+}
 /*
 *	Implement the equals function that takes an Address object and checks if it is equal to
 *	current object refered by (this pointer)
@@ -21,23 +51,32 @@
 */
 bool Address::equals(const Address& address)
 {
-	/*
-	* Todo
-	*/
+	if (this->house == address.house && this->street == address.street && this->city == address.city && this->country == address.country)
+	{
+		return true;
+	}
+	
 	return false;
 }
 
 void Address::print_address()
 {
-	/*
-	*	Print address on one line as
-	*	House, Street, City, Country
-	*/
+	std::cout << house;
+	std::cout << street;
+	std::cout << city;
+	std::cout << country;
 }
 
-Address Address::copy_address()
+Address *Address::copy_address()
 {
-	/*
-		return a copy of this
-	*/
+	Address *obj;
+	obj = new Address;
+	obj -> house = this -> house;
+	obj -> street = this -> street;
+	obj -> city = this -> city;
+	obj -> country =this -> country;
+	return obj;
+}
+Address::Address() {
+
 }
