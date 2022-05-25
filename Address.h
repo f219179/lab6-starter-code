eceb17ad0143f2928b48d7e530096320efd35ba7
f219179 <101401415+f219179@users.noreply.github.com>
2022-05-26 01:42:35 +0500

    Update Address.h

--- Address.h
+++ Address.h
@@ -1,19 +1,24 @@
 #pragma once
 #include <iostream>
 #include <string>
-
 class Address {
 private:
 	std::string house;
 	std::string street;
 	std::string city;
 	std::string country;
-
 public:
+	Address(std::string house, std::string street, std::string city, std::string country);
+	void set_house(std::string house);
+	void set_street(std::string street);
+	void set_city(std::string city);
+	void set_country(std::string country);
+	std::string get_house();
+	std::string get_street();
+	std::string  get_city();
+	std::string get_country();
 	bool equals(const Address& address);
 	void print_address();
-	Address copy_address();
-
-	Address(std::string house, std::string street, std::string city, std::string country);
-	Address() {}
+	Address *copy_address();
+	Address();
 };
